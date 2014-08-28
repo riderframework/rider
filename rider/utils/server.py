1ab38d64c1cded9d8563d20fd090f6afe5fd32d3
Jan Češpivo <jan.cespivo@gmail.com>
2014-08-28 16:13:22 +0200

    update dev server + manifest file

--- rider/utils/server.py
+++ rider/utils/server.py
@@ -1,6 +1,8 @@
+from rider import application
 from wsgiref.simple_server import make_server
+import sys
 
-def run(application):
+def run(args=sys.argv):
     try:
         print('Visit http://localhost:8080/')
         make_server('', 8080, application).serve_forever()
