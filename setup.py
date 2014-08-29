970d0615ce18e7cd787f91fecf0ce2c809a9344a
Jan Češpivo <jan.cespivo@gmail.com>
2014-08-29 10:21:14 +0200

    version update

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ DESCRIPTION = "Python Web Application Framework"
 AUTHOR = "Jan Češpivo (http://www.cespivo.cz/)"
 AUTHOR_EMAIL = "jan.cespivo@gmail.com"
 URL = "http://www.riderframework.com/"
-VERSION = '0.0.1a'
+VERSION = '0.1.0a'
 REQUIRES = ['falcon']
 
 setup(
