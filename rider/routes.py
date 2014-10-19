0ceeca8e7077511b453e261f108f28551ec2c8cd
Jan Češpivo <jan.cespivo@gmail.com>
2014-10-19 10:03:41 +0200

    viewsets support

--- rider/routes.py
+++ rider/routes.py
@@ -1,8 +1,9 @@
 import falcon
 
-from rider.views import View
-from rider.utils import import_module, import_object
+from rider.views import View, ViewSet
 
+from rider.utils import import_module, import_object
+from types import ModuleType
 
 application = falcon.API()
 
@@ -10,27 +11,46 @@ application = falcon.API()
 BASE_URL = []
 
 
-def include_routes(url, module, namespace=''):
+def include_routes(url, viewset_or_module, namespace=''):
     BASE_URL.append(url)
-    import_module(module)
+    try:
+        viewset = import_object(viewset_or_module)
+        try:
+            if issubclass(viewset, ViewSet):
+                for subview in viewset().get_views():
+                    route('', subview)
+            else:
+                #TODO revise text
+                raise Exception('bad class')
+        except TypeError:
+            #TODO revise text
+            raise Exception('not class')
+    except AttributeError:
+        #ordinary module
+        import_module(viewset_or_module)
     del BASE_URL[-1]
 
 
-def route(url, view=None, name=''):
+def route(url, view=None, name='', is_method=False):
     #decorator wrapper
     if not view:
         def route_wrapper(cls):
-            route(url, view=cls, name=name)
+            cls.add_url(url, name)
             return cls
         return route_wrapper
 
-    if type(view) == str:
+    if isinstance(view, str):
         view = import_object(view)
     try:
-        if not issubclass(view, View):
-            # TODO explanation
-            raise Exception()
-    except TypeError:
-        # TODO explanation
-        raise Exception()
-    application.add_route('%s%s' % (''.join(BASE_URL), url), view())
+        if issubclass(view, View):
+            if url:
+                application.add_route('%s%s' % (''.join(BASE_URL), url), view())
+            for cls_url, cls_url_name in view.get_urls():
+                application.add_route('%s%s' % (''.join(BASE_URL), cls_url), view())
+        else:
+            #TODO revise text
+            raise Exception('bad class')
+    except TypeError as e:
+        #TODO revise text
+        raise Exception('not class: %s' % e)
+
--- rider/views.py
+++ rider/views.py
@@ -1,6 +1,6 @@
 import falcon
 import json
-from functools import wraps
+from functools import wraps, partial
 from rider.exceptions import HttpException
 from rider.response import ResponseSetter
 
@@ -12,6 +12,15 @@ class View(ResponseSetter):
     Wrapper around falcon view api
     '''
     exceptions_mimic = True
+    urls = {}
+
+    @classmethod
+    def get_urls(cls):
+        return cls.urls.get(cls, [])
+
+    @classmethod
+    def add_url(cls, url, name):
+        cls.urls.setdefault(cls, []).append((url, name))
 
     def __init__(self):
         super(View, self).__init__()
@@ -25,7 +34,7 @@ class View(ResponseSetter):
                         getattr(self, method_name)
                     )
                 )
-            except AttributeError:
+            except AttributeError as e:
                 pass
 
     def _wrap_response(self, method):
@@ -80,16 +89,39 @@ class JsonView(TextView):
     content_wrapper = json.dumps
 
 
-def view(ViewClass):
-    '''
-    decorator providing support for functional views
-    '''
-    def wrapper(func):
-        class FunctionView(ViewClass):
-            def __init__(self):
-                for http_method in falcon.HTTP_METHODS:
+class view(object):
+    def __init__(wrapper, view_class, http_method='GET'):
+        wrapper.view_class = view_class
+        if isinstance(http_method, str):
+            wrapper.http_methods = [http_method]
+        elif isinstance(http_method, list):
+            wrapper.http_methods = http_method
+        else:
+            #TODO revise text
+            raise Exception('http method definition must be string or list')
+
+    def __call__(wrapper, func):
+        class FunctionView(wrapper.view_class):
+            viewset = None
+            def __init__(instance):
+                for http_method in wrapper.http_methods:
+                    if http_method not in falcon.HTTP_METHODS:
+                        raise Exception('%s is not in %s' (http_method, falcon.HTTP_METHODS))
                     method_name = http_method.lower()
-                    setattr(self, method_name, func)
-                super(FunctionView, self).__init__()
+                    setattr(instance, method_name, wraps(func)(partial(func, instance)) if instance.viewset else func)
+                super(FunctionView, instance).__init__()
         return FunctionView
-    return wrapper
+
+
+class ViewSet(object):
+    def get_views(self):
+        for attr_name in dir(self):
+            attr = getattr(self, attr_name)
+            if callable(attr):
+                try:
+                    if issubclass(attr, View):
+                        attr.viewset = self
+                        yield attr
+                except TypeError:
+                    #not class
+                    continue
