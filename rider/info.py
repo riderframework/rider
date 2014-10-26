3ac47efddbaeb50a9d7c705674946c015b6a5b67
Jan Češpivo <jan.cespivo@gmail.com>
2014-10-26 15:55:05 +0100

    version fix

--- rider/info.py
+++ rider/info.py
@@ -5,8 +5,5 @@ DESCRIPTION = "Python Web Application Framework"
 AUTHOR = "Jan Češpivo (http://www.cespivo.cz/)"
 AUTHOR_EMAIL = "jan.cespivo@gmail.com"
 URL = "http://www.riderframework.com/"
-<<<<<<< HEAD
 VERSION = '0.1.8a'
-=======
-VERSION = '0.1.7a'
->>>>>>> 976142ec2252a22f3c59cfbe41c282e4330355a7
+
