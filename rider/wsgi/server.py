4c715ad70545427da7d4f73d5c42397576ade7ec
Jan Češpivo <jan.cespivo@gmail.com>
2014-11-25 21:53:22 +0100

    change wsgi server to gevent

--- rider/wsgi/server.py
+++ rider/wsgi/server.py
@@ -1,24 +1,16 @@
 from rider.wsgi import application
-from gunicorn.app.base import BaseApplication
+from gevent.wsgi import WSGIServer
 
 
-class WsgiServer(BaseApplication):
-    def __init__(self, host='127.0.0.1', port=8080):
+class WsgiServer(object):
+    def __init__(self, host='127.0.0.1', port=8000):
         self.host = host
         self.port = port
         self.application = application
         super(WsgiServer, self).__init__()
 
-    def load_config(self):
-        self.cfg.set('bind', '%s:%s' % (self.host, self.port))
-        self.cfg.set('workers', 2)
-        #self.cfg.set('logger_class', 'logging.Logger')
-
-    def load(self):
-        return self.application
-
     def start(self):
-        self.run()
+        WSGIServer((self.host, self.port), self.application, spawn=None).serve_forever()
 
     def stop(self):
         pass
